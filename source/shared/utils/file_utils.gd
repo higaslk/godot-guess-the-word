0dfd21d8715da2060968add737f4c2077b26eafa
Higaslk <iicarlosgomes477@gmail.com>
2025-11-06 23:12:36 -0300

    feat(file utils) utility script for file search.

---
+++ source/shared/utils/file_utils.gd
@@ -0,0 +1,18 @@
+class_name FileUtils
+extends RefCounted
+
+
+static func get_files_at(dir_path: String, extension: String) -> Array:
+    var dir: DirAccess = DirAccess.open(dir_path)
+    if not dir: return []
+
+    var dir_files: PackedStringArray = dir.get_files()
+    var files: Array = []
+
+    for file_name: String in dir_files:
+        if files.has(file_name): continue
+        if not file_name.ends_with(extension): continue
+
+        files.append(file_name)
+    print(files)
+    return files
